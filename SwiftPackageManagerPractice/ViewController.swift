c5fa7d49f33020518d495db54b5c0b7e6adf728b
Masateru maegawa <masa.mg283@gmail.com>
2021-03-08 22:48:26 +0900

    Initial Commit

---
+++ SwiftPackageManagerPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPackageManagerPractice
+//
+//  Created by Masateru Maegawa on 2021/03/08.
+//  Copyright © 2021 Masateru Maegawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
